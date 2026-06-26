93e5cbcce54a6e7868e35049db6861c94d912e13
Vipin Mishra <vipinmishra2302@gmail.com>
2026-06-26 11:01:06 +0530

    Program for parking_fee_waiver

--- Classwork/day3/ElectricityBill_Discount.py
+++ Classwork/day3/ElectricityBill_Discount.py
@@ -5,30 +5,43 @@ An electricity provider offers a 10% discount on the total bill amount if the cu
   
  Write a Python program to accept the total bill amount from the user and 
  display the final amount to be paid. 
+ ----------------------------------
+Sample Input 1 
+Enter the electricity bill amount: 6200 
+------------------------------------
+Sample Output 1 
+Discount Applied! Final Bill Amount: ₹5580.0 
+-------------------------------------
+Sample Input 2 
+Enter the electricity bill amount: 4200
+---------------------------------------
+Sample Output 2 
+No Discount Applied! Final Bill Amount: ₹4200 
+--------------------------------------------------------------
 '''
 
 # ------Coding-------
 
 #input of bill from the user
-bill = int(input("Enter the bill amount (in Rs): "))
+bill = int(input("Enter the electricity bill amount: "))
 
 #validate the bill
 if(bill>=0):
     if(bill>=5000):
          discounted_amount = bill/10
          bill = bill - discounted_amount
-         print("Your discounted bill (in Rs): ", bill)
+         print("Discount Applied! Final Bill Amount: ", bill)
     else:
-         print("Your bill (in Rs): ",bill)
+         print("No Discount Applied! Final Bill Amount: ",bill)
 
 else:
      print("Please enter valid amount !!!")
      
 #----------------------------------------------------
 '''Output : 
-Enter the bill amount (in Rs): 8762
+Enter the electricity bill amount: 8762
 --------------------------------------------
-Your discounted bill (in Rs):  7885.8
+Discount Applied! Final Bill Amount: 7885.8
 --------------------------------------------'''
 
 
