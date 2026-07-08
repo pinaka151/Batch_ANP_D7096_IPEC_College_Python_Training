ebd8c9e77700ceb04284c60d50a2e4e09eb5a716
Vipin Mishra <vipinmishra2302@gmail.com>
2026-07-08 19:07:31 +0530

    All functions created to use as module

---
+++ Classwork/day11/numeric_calculation.py
@@ -0,0 +1,22 @@
+# function to add two numbers
+
+def Calculation_addition(a,b):
+    return (a + b)
+
+# function to subtract two number
+def Calculation_subtraction(a,b):
+    return (a - b)
+
+# function to multiply two number
+def Calculation_multiplication(a,b):
+    return (a * b)
+
+# function to divide two number
+def Calculation_divition(a,b):
+    if b == 0 :
+        exit("Please enter valid number as b can't be 0")
+    return (a / b)
+
+# function to get remainder between two number
+def Calculation_remainder(a,b):
+    return (a % b)
