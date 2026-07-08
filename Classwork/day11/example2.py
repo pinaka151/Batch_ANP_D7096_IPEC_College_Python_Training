a4593ea5734bf3a6bf2e3d2ae13426c4cf88e041
Vipin Mishra <vipinmishra2302@gmail.com>
2026-07-08 19:04:21 +0530

    program to import all  functions from other file using from keyword

---
+++ Classwork/day11/example2.py
@@ -0,0 +1,21 @@
+# importing module
+
+from numeric_calculation import *
+
+a = 13
+b = 5
+
+print(f"Sum of {a} and {b} = ",Calculation_addition(a,b))
+print(f"Differnce between {a} and {b} = ",Calculation_subtraction(a,b))
+print(f"Multiplication of {a} and {b} = ",Calculation_multiplication(a,b))
+print(f"Divition of {a} with {b} = ",Calculation_divition(a,b))
+print(f"Remainder of {a} when divide by {b} = ",Calculation_remainder(a,b))
+
+'''-------------Output--------------------------
+Sum of 5 and 10 =  18
+Differnce between 5 and 10 =  8
+Multiplication of 5 and 10 =  65
+Divition of 5 with 10 =  2.6
+Remainder of 5 when divide by 10 =  3
+------------------------------------------------
+'''
